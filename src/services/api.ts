f7d25f1854c88dd795b1dac5b58e7021ceb58281
Lucas Cavalcante <lucas.cavalcant.06@gmail.com>
2022-03-14 22:56:30 -0300

    update files

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,12 +7,14 @@ export const apiUrl = axios.create({
     baseURL: "http://localhost:3000/api/v1",
 })
 
-apiUrl.interceptors.response.use(config => {
-    // console.log(config.data.movies[0].title)
-
-    return config
+apiUrl.interceptors.request.use((config) => {
+    config.params = {
+        api_key: process.env.API_KEY, ...config.params }; // tmdb
+        return config;
 })
 
 if(token) {
     apiUrl.defaults.headers['Authorization'] = `Bearer ${token}`
 }
+
+export default apiUrl
